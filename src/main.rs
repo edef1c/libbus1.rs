a5e551cd5c981a1ac3f4a7ecceb61e0f9f181f62
edef <edef@edef.eu>
2017-10-24 20:28:22 +0200

    make the example actually output "hello, world"

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate bus1;
 extern crate libc;
 
+use std::str;
+
 fn main() {
     let peer1 = bus1::Peer::new().expect("couldn't create peer1");
     let peer2 = bus1::Peer::new().expect("couldn't create peer2");
 
     let handle2 = peer1.transfer_handle(0, &peer2).expect("couldn't transfer handle");
-    peer2.send(&[handle2], b"hello, world", &[], &[]).expect("couldn't send message");
+    peer2.send(&[handle2], "hello, world".as_bytes(), &[], &[]).expect("couldn't send message");
     match peer1.recv().expect("couldn't receive message") {
-        bus1::Message::Data(msg) => println!("{:?}", msg.payload()),
+        bus1::Message::Data(msg) => println!("{}", str::from_utf8(msg.payload()).unwrap()),
         _ => unreachable!()
     };
 }
