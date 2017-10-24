4e28d8eb2a8cc258acad8fb93e8a70b091387c8e
edef <edef@edef.eu>
2017-10-24 18:35:01 +0200

    clean up the example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 extern crate bus1;
 extern crate libc;
 
-use std::io;
-
 fn main() {
     let peer1 = bus1::Peer::new().expect("couldn't create peer1");
     let peer2 = bus1::Peer::new().expect("couldn't create peer2");
 
     let handle2 = peer1.transfer_handle(0, &peer2).expect("couldn't transfer handle");
     peer2.send(&[handle2], b"hello, world", &[], &[]).expect("couldn't send message");
-    let msg = peer1.recv().expect("couldn't receive message");
-    match msg {
+    match peer1.recv().expect("couldn't receive message") {
         bus1::Message::Data(msg) => println!("{:?}", msg.payload()),
         _ => unreachable!()
-    }
+    };
 }
